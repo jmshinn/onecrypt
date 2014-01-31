7b8249ec03858b4976d6d22953594fe485ffaebc
Jason Shinn <jason.shinn@cablesandkits.com>
2014-01-31 11:34:02 -0500

    complete test suite for all current functionality

--- test/test-onecrypt.js
+++ test/test-onecrypt.js
@@ -107,6 +107,7 @@ exports.kdfs = {
 			});
 		});
 	},
+	// these are simple checks to make sure we've properly set up the encrypt/verify pairs in the case of each kdf
 	bcrypt_kdf: function (test) {
 		test.expect(1);
 
@@ -149,8 +150,12 @@ exports.kdfs = {
 		test.done();
 	},
 	/*
+	tests for the kdfs using the module to encrypt and onecrypt to verify, and vice versa
+	*/
+	/*
 	tests for the kdfs with different options
 	*/
+	// just verify that our function produces the same output as the built in
 	internal_pbkdf2: function (test) {
 		test.expect(1);
 
@@ -167,7 +172,7 @@ exports.kdfs = {
 };
 
 exports.hashes = {
-	default_hash: function (test) {
+	default_hash_message: function (test) {
 		test.expect(1);
 
 		var digestSync = onecrypt.digest(message);
@@ -175,5 +180,79 @@ exports.hashes = {
 		test.ok(resultSync, 'we successfully verified our password against the hash. simple');
 
 		test.done();
+	},
+	// these are simple checks to make sure we've properly set up the digest/match pairs
+	buncha_hashes_message: function (test) {
+		var hashes = ['md5', 'sha1', 'sha256', 'whirlpool'];
+		test.expect(hashes.length*2-1);
+
+		var hash, digest, result;
+		var last_digest;
+
+		for (var i=0; i<hashes.length; i++) {
+			hash = new onecrypt.hash(null, hashes[i]);
+
+			// there's no reason to do this asynchronously because we haven't requested to generate a salt
+			digest = hash.digest(message);
+			if (last_digest) test.notEqual(last_digest, digest, 'the different algorithm should produce a different output');
+			last_digest = digest;
+
+			result = hash.match(message, digest);
+
+			test.ok(result, 'our digest/match pair successfully worked together');
+		}
+
+		test.done();
+	},
+	magento_style_password: function (test) {
+		test.expect(2);
+
+		// magento hashes the password with md5 with a two character hash, which is appended to the password with a colon delimiter
+		// we need to specify both a byte length for the salt, so we can generate it, and a character length, in case the bytes resolve
+		// to more than 2 characters
+		// the default encoding is already hex, but make it explicit lest our default should change
+		// technically, with salt generation, we could/should be doing this asynchronously, but I can't be bothered to make an md5 hash
+		// all that efficient
+		var mag = new onecrypt.hash({ salt: true, salt_byte_length: 2, salt_char_length: 2, salt_after: true, delim: ':', encoding: 'hex' }, 'md5');
+		var pass = mag.digest(password);
+		test.ok(pass.match(/^[a-fA-F0-9]{32}:[a-fA-F0-9]{2}$/), 'hexadecimal with two character salt appended');
+		var result = mag.match(password, pass);
+		test.ok(result, 'we matched with our matched matcher');
+
+		test.done();
+	},
+	sha1_w_6char_salt_password: function (test) {
+		test.expect(4);
+
+		var sha1 = new onecrypt.hash({ salt: true, salt_byte_length: 6, salt_char_length: 6, delim: '$' }, 'sha1');
+
+		var pass = sha1.digest(password);
+		test.ok(pass.match(/^[a-fA-F0-9]{6}\$[a-fA-F0-9]{40}$/), 'hexadecimal with 6 character salt prepended');
+		var result = sha1.match(password, pass);
+		test.ok(result, 'we matched with our matched matcher');
+
+		sha1 = new onecrypt.hash({ salt: true, salt_byte_length: 6, salt_char_length: 6, concat: true }, 'sha1');
+		pass = sha1.digest(password);
+		test.ok(pass.match(/^[a-fA-F0-9]{46}$/), 'hexadecimal with 6 character salt prepended, no delimiter');
+		result = sha1.match(password, pass);
+		test.ok(result, 'we matched, with our matched matcher knowing how to pull off the salted bits');
+
+		test.done();
+	},
+	upgrade_digest_to_encrypt_password: function (test) {
+		test.expect(3);
+
+		// we've provided a convenience definition for certain common hashes
+		var mag = new onecrypt.hash(onecrypt.upgrade_magento.opts, onecrypt.upgrade_magento.algorithm);
+		var pass = mag.digest(password);
+
+		var result = onecrypt.upgrade_hash(password, pass, [mag]);
+		test.notStrictEqual(result, false, 'we should not get boolean false');
+		test.notStrictEqual(result, true, 'we should not get boolean true');
+
+		var result1 = onecrypt.verify(password, result, function(err, result2) {
+			test.strictEqual(result2, true);
+			test.done();
+		});
 	}
 };
